f5e73f00dea3cc1b789ebd4c488ab6bc2f355402
fadhil.diya <fadhil.diya@binawan.ac.id>
2022-08-28 23:25:36 +0700

    initial commit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,31 +1,36 @@
 <div class="sidebar pe-4 pb-3">
-        <nav class="navbar bg-secondary navbar-dark">
-            <a href="/" class="navbar-brand mx-4 mb-3">
-                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
-            </a>
-            <div class="d-flex align-items-center ms-4 mb-4">
-                <div class="position-relative">
-                    <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                    <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
-                    </div>
-                </div>
-                <div class="ms-3">
-                    <h6 class="mb-0">Administrator</h6>
+    <nav class="navbar bg-secondary navbar-dark">
+        <a href="/" class="navbar-brand mx-4 mb-3">
+            <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
+        </a>
+        <div class="d-flex align-items-center ms-4 mb-4">
+            <div class="position-relative">
+                <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                <div
+                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
                 </div>
             </div>
-            <div class="navbar-nav w-100">
-                <a class="nav-item nav-link {{ ($title ==="Dashboard") ? 'active' : '' }}" href="/"><i class="fa fa-chart-line me-2"></i>Dashboard</a>
-                <a class="nav-item nav-link {{ ($title ==="Order List") ? 'active' : '' }}" href="/order-list"><i class="fa fa-list me-2"></i>Order Lists</a>
-                <a class="nav-item nav-link {{ ($title ==="Products") ? 'active' : '' }}" href="/products"><i class="fa fa-box alt me-2"></i>Products</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-list me-2"></i>Data Master</a>
-                    <div class="dropdown-menu bg-transparent border-0">
-                        <a href="button.html" class="dropdown-item">User</a>
-                        <a href="typography.html" class="dropdown-item">Type Items</a>
-                        <a href="element.html" class="dropdown-item">Source Payment</a>
-                        <a href="element.html" class="dropdown-item">Supplier</a>
-                    </div>
+            <div class="ms-3">
+                <h6 class="mb-0">Administrator</h6>
+            </div>
+        </div>
+        <div class="navbar-nav w-100">
+            <a class="nav-item nav-link {{ $title === 'Dashboard' ? 'active' : '' }}" href="/"><i
+                    class="fa fa-chart-line me-2"></i>Dashboard</a>
+            <a class="nav-item nav-link {{ $title === 'Order List' ? 'active' : '' }}" href="/order-list"><i
+                    class="fa fa-list me-2"></i>Order Lists</a>
+            <a class="nav-item nav-link {{ $title === 'Products' ? 'active' : '' }}" href="/products"><i
+                    class="fa fa-box alt me-2"></i>Products</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
+                        class="fa fa-list me-2"></i>Data Master</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="button.html" class="dropdown-item">User</a>
+                    <a href="typography.html" class="dropdown-item">Type Items</a>
+                    <a href="element.html" class="dropdown-item">Source Payment</a>
+                    <a href="element.html" class="dropdown-item">Supplier</a>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+</div>
