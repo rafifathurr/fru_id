cc21d807d424a67fb22642e21eafd67df680120a
rafifathurr <rafifathur.rahman20@gmail.com>
2022-08-28 12:17:14 +0700

    UPDATE html to LARAVEL

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layouts.head')
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+        <!-- Spinner Start -->
+        <!-- <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div> -->
+        <!-- Spinner End -->
+        @include('layouts.sidebar')
+        <div class="content">
+            @include('layouts.navbar')
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-6 col-xl-6">
+                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-dollar-sign fa-3x text-primary"></i>
+                            <div class="ms-3">
+                                <p class="mb-2">Total Profit</p>
+                                <div class="d-flex">
+                                    <i class="fa fa-arrow-up text-primary" style='margin-right:10px;color:green !important;'></i>
+                                    <h6 class="mb-0">Rp.500.000.000,-</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-6 ">
+                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-dollar-sign fa-3x text-primary"></i>
+                            <div class="ms-3">
+                                <p class="mb-2">Latest Profit</p>
+                                <h6 class="mb-0">Rp.200.000.000,-</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="col-sm-6 col-xl-3">
+                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                            <div class="ms-3">
+                                <p class="mb-2">Total Sale</p>
+                                <h6 class="mb-0">$1234</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-area fa-3x text-primary"></i>
+                            <div class="ms-3">
+                                <p class="mb-2">Today Revenue</p>
+                                <h6 class="mb-0">$1234</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-3">
+                        <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                            <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                            <div class="ms-3">
+                                <p class="mb-2">Total Revenue</p>
+                                <h6 class="mb-0">$1234</h6>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+            <!-- Sale & Revenue End -->
+            <!-- Sales Chart Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-secondary text-center rounded p-4">
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h6 class="mb-0">Sales Profit by Year</h6>
+                            </div>
+                            <canvas id="profityear-sales"></canvas>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-secondary text-center rounded p-4">
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h6 class="mb-0">Sales Profit by Month of The Year </h6>
+                            </div>
+                            <canvas id="month-sales"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Sales Chart End -->
+        </div>
+
+        <!-- Sidebar Start -->
+    </div>
+    @include('layouts.footer')
+    </body>
+</html>
