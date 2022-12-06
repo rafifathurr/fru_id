df550ed567b3749d4d7aff35b916301f057b424e
fadhil.diya <fadhil.diya@binawan.ac.id>
2022-12-06 19:58:32 +0800

    form product

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 @include('layouts.head')
+
 <body>
     <div class="wrapper">
         <div class="main-header">
@@ -13,72 +14,25 @@
                     <div class="page-inner py-5">
                         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                             <div>
-                                <h2 class="text-white pb-2 fw-bold">{{$title}}</h2>
+                                <h2 class="text-white pb-2 fw-bold">{{ $title }}</h2>
                             </div>
                         </div>
                     </div>
                 </div>
                 <section class="container">
-                    <form id="form_add" action="{{ route('product.' . $url) }}" method="post" enctype="multipart/form-data" style="margin-right:100px;">
-                    {{ csrf_field() }}
-                        <br>
-                        <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Username <span style="color: red;">*</span></label>
-                                <div class="col-md-10">
-                                    {{-- <input type="hidden" class="form-control" id="id" name="id" autocomplete="off" @isset($users) value="{{ $users->id }}" readonly @endisset required> --}}
-                                    <input type="text" name="username" id="username" class="form-control"  step="1" {{-- @if (isset($users)) value="{{ $users->username }}" @endisset --}} autocomplete="off" required {{ $disabled_ }} style="width:100%;">
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Email <span style="color: red;">*</span></label>
-                                <div class="col-md-10">
-                                    <input type="text" name="email" id="email" class="form-control"  step="1" {{-- @if (isset($users)) value="{{ $users->email }}" @endisset --}} autocomplete="off" required {{ $disabled_ }} style="width:100%;">
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Phone <span style="color: red;">*</span></label>
-                                <div class="col-md-10">
-                                    <input type="number" name="phone" id="phone" class="form-control"  step="1" {{-- @if (isset($users)) value="0{{ $users->phone }}" @endisset --}} autocomplete="off" required {{ $disabled_ }} style="width:100%;">
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Password <span style="color: red;">*</span></label>
-                                <div class="col-md-10">
-                                    <input type="password" name="password" id="password" class="form-control"  step="1" autocomplete="off" required {{ $disabled_ }} style="width:100%;">
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Re-Password <span style="color: red;">*</span></label>
-                                <div class="col-md-10">
-                                    <input type="password" name="repassword" id="repassword" class="form-control"  step="1" autocomplete="off" required {{ $disabled_ }} style="width:100%;">
-                                </div>
-                            </div>
-                        </div>
+                    <form id="form_add" action="{{ route('product.' . $url) }}" method="post"
+                        enctype="multipart/form-data" style="margin-right:100px;">
+                        {{ csrf_field() }}
                         <br>
                         <div class="row">
                             <div class="col-md-10">
                                 <div class="col-md-2"></div>
                                 <label class="col-md-2">Name <span style="color: red;">*</span></label>
                                 <div class="col-md-10">
-                                    <input type="text" name="name" id="name" class="form-control"  step="1" {{-- @if (isset($users)) value="{{ $users->name }}" @endisset --}} autocomplete="off" required {{ $disabled_ }} style="width:100%;">
+                                    {{-- <input type="hidden" class="form-control" id="id" name="id" autocomplete="off" @isset($users) value="{{ $users->id }}" readonly @endisset required> --}}
+                                    <input type="text" name="username" id="username" class="form-control"
+                                        step="1" {{-- @if (isset($users)) value="{{ $users->username }}" @endisset --}} autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%;">
                                 </div>
                             </div>
                         </div>
@@ -86,33 +40,88 @@
                         <div class="row">
                             <div class="col-md-10">
                                 <div class="col-md-2"></div>
-                                <label class="col-md-2">Role <span style="color: red;">*</span></label>
+                                <label class="col-md-2">Status <span style="color: red;">*</span></label>
+                                <div class="col-md-6">
+                                    <input type="text" name="name" id="name" class="form-control"
+                                        step="1" {{-- @if (isset($users)) value="{{ $users->name }}" @endisset --}} autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%;">
+                                </div>
+                                <label class="col-md-1 mt-1">Stock <span style="color: red;">*</span></label>
+                                <div class="col-md-3">
+                                    <input type="number" name="name" id="name" class="form-control"
+                                        step="1" {{-- @if (isset($users)) value="{{ $users->name }}" @endisset --}} autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%;">
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="col-md-2"></div>
+                                <label class="col-md-2">Base Price <span style="color: red;">*</span></label>
+                                <div class="col-md-4">
+                                    <input type="number" name="name" id="name" class="form-control"
+                                        step="1" {{-- @if (isset($users)) value="{{ $users->name }}" @endisset --}} autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%;">
+                                </div>
+                                <label class="col-md-2 mt-1">Selling Price <span style="color: red;">*</span></label>
+                                <div class="col-md-4">
+                                    <input type="number" name="name" id="name" class="form-control"
+                                        step="1" {{-- @if (isset($users)) value="{{ $users->name }}" @endisset --}} autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%;">
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="col-md-2"></div>
+                                <label class="col-md-2">Description <span style="color: red;">*</span></label>
                                 <div class="col-md-10">
-                                    <select class="form-control" name="role" id="role"  {{-- @if (isset($users)) @endisset --}} autocomplete="off" required {{ $disabled_ }}>
-                                        <option value="" selected disabled hidden>- Select Role -</option>
-                                        {{-- @foreach($roles as $role)
-                                            <option  @if(isset($users)) <?php if($users->role_id == $role->id){echo 'selected';}?> @endisset value="{{$role->id}}">{{$role->role}}</option>
-                                        @endforeach --}}
+                                    <textarea class="form-control" name="address" id="address" rows="5" cols="10" autocomplete="off" required
+                                        {{ $disabled_ }} style="width:100%">{{-- @if (isset($users)) {{ $users->address }} @endisset --}}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-10">
+                                <div class="col-md-2"></div>
+                                <label class="col-md-2">Category <span style="color: red;">*</span></label>
+                                <div class="col-md-4">
+                                    <select class="form-control selectpicker" id="communityServiceSchemeId"
+                                        name="communityServiceSchemeId" data-size="8" data-show-subtext="true"
+                                        data-live-search="true">
+                                        <option value="">-Please Select-</option>
+                                    </select>
+                                </div>
+                                <label class="col-md-2 mt-1">Supplier <span style="color: red;">*</span></label>
+                                <div class="col-md-4">
+                                    <select class="form-control selectpicker" id="communityServiceSchemeId"
+                                        name="communityServiceSchemeId" data-size="8" data-show-subtext="true"
+                                        data-live-search="true">
+                                        <option value="">-Please Select-</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
                         <br>
                         <div class="row">
-                            <div class="col-md-10">
-                                <div class="col-md-2"></div>
-                                <label class="col-md-2">Address <span style="color: red;">*</span></label>
                                 <div class="col-md-10">
-                                    <textarea class="form-control" name="address" id="address" rows="5" cols="10"  autocomplete="off" required {{ $disabled_ }} style="width:100%">{{-- @if (isset($users)) {{ $users->address }} @endisset --}}</textarea>
+                                    <div class="col-md-2"></div>
+                                    <label class="col-md-2">Attachment <span style="color: red;">*</span></label>
+                                    <div class="col-md-10">
+                                        <input type="file" id="fileAttachment" name="fileAttachment"
+                                            accept="application/pdf">
+                                    </div>
                                 </div>
-                            </div>
                         </div>
                         <br>
                         <div class="modal-footer">
                             <div style="float:right;">
                                 @if ($title == 'Add Products')
                                     <div class="col-md-10" style="margin-right: 20px;">
-                                        <a href="{{ route('users.index')}}" type="button" class="btn btn-danger">
+                                        <a href="{{ route('users.index') }}" type="button" class="btn btn-danger">
                                             <i class="fa fa-arrow-left"></i>&nbsp;
                                             Back
                                         </a>
@@ -123,7 +132,7 @@
                                     </div>
                                 @elseif ($title == 'Edit Products')
                                     <div class="col-md-10" style="margin-right: 20px;">
-                                        <a href="{{ route('users.index')}}" type="button" class="btn btn-danger">
+                                        <a href="{{ route('users.index') }}" type="button" class="btn btn-danger">
                                             <i class="fa fa-arrow-left"></i>&nbsp;
                                             Back
                                         </a>
@@ -134,7 +143,7 @@
                                     </div>
                                 @else
                                     <div class="col-md-10" style="margin-right: 20px;">
-                                        <a href="{{ route('users.index')}}" type="button" class="btn btn-danger">
+                                        <a href="{{ route('users.index') }}" type="button" class="btn btn-danger">
                                             <i class="fa fa-arrow-left"></i>&nbsp;
                                             Back
                                         </a>
@@ -149,4 +158,5 @@
         </div>
     </div>
 </body>
+
 </html>
